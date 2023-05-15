37cc93930bc386e6d73f0335124ac38ccbf67f1d
Айрат Каримов <airatkarimov999@gmail.com>
2023-05-15 14:53:02 +0300

    Добавьте файлы проекта.

---
+++ DotaScope/Data/DotaAPI.cs
@@ -0,0 +1,30 @@
+﻿using SteamWebAPI2.Interfaces;
+using SteamWebAPI2.Utilities;
+
+namespace DotaScope.Data
+{
+    public class DotaAPI
+    {
+        private SteamWebInterfaceFactory webApi;
+        private DOTA2Match dotaMatch;
+
+        public DotaAPI(string webApiKey)
+        {
+            this.webApi = new SteamWebInterfaceFactory(webApiKey);
+            dotaMatch = webApi.CreateSteamWebInterface<DOTA2Match>();
+        }
+
+        public DOTA2Match GetDOTA2 { get => dotaMatch; }
+
+        public async Task<IReadOnlyCollection<Steam.Models.DOTA2.LiveLeagueGameModel>> GetLiveLigueDataAsync()
+        {
+            var liveGames = await dotaMatch.GetLiveLeagueGamesAsync();
+            return liveGames.Data;
+        }
+
+        public async Task<ISteamWebResponse<IReadOnlyCollection<Steam.Models.DOTA2.TeamInfo>>> GetTeams()
+        {
+            return await dotaMatch.GetTeamInfoByTeamIdAsync();
+        }
+    }
+}
---
+++ DotaScope/Data/SingletonDotaApi.cs
@@ -0,0 +1,22 @@
+﻿namespace DotaScope.Data
+{
+    public class SingletonDotaApi
+    {
+        DotaAPI dotaAPI;
+
+        public SingletonDotaApi()
+        {
+            this.dotaAPI = new DotaAPI("170B3F92C5C2F9C7A2D8BC7DC3FB97F7");
+        }
+
+        public DotaAPI GetDotaAPI()
+        {
+            return dotaAPI;
+        }
+
+        public void SetDotaAPI(DotaAPI dotaAPI)
+        {
+            this.dotaAPI = dotaAPI;
+        }
+    }
+}
